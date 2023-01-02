ab1061a0e740b4c2a940a58d43e9b4a488542b0d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-02 21:27:59 +0600

    theory of compile-time constants

---
+++ Runtime and Compile-time/theory/theory_of_compile-time_constants.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/runtime-and-compile-time-constants-in-c/
+                  https://www.javatpoint.com/compile-time-vs-runtime
+                  https://nus-cs1010.github.io/2223-s1/14-array.html
+                  https://www.learncpp.com/cpp-tutorial/compile-time-constants-constant-expressions-and-constexpr/
+                  https://www.learncpp.com/cpp-tutorial/arrays-part-i/
+                  https://www.techrepublic.com/article/from-compile-time-constants-to-runtime-constants-and-back/
+                  https://www.geeksforgeeks.org/memory-layout-of-c-program/
+                  https://www.baeldung.com/cs/runtime-vs-compile-time
+*/
+/*
+    Compile-time is the time at which the source code is converted into an executable code
+    while the run time is the time at which the executable code is started running.
+    Both the compile-time and runtime refer to different types of error.
+*/
+/*
+    Compile-time Constant:
+    These are the constants whose respective value is known or computed at the time of compilation of source code.
+    Compile-time constants are faster than run-time constants but are less flexible than run-time constants.
+
+    A Compile-time constant is a constant whose value is known at compile-time. Literals (e.g. ‘1’, ‘2.3’, and
+    “Hello, world!”) are one type of compile-time constant.
+
+    like an example:
+    const int x = 3;  // x is a compile-time const
+*/
+/*
+    Inputs and Outputs:
+    Inputs and outputs during compile time are the following:
+    1.Inputs – Source code, dependent files, interfaces, libraries required for successful compilation of the code
+    2.Outputs – On successful compilation, a complied code (assembly code or relocatable object code), otherwise
+    compile time error messages
+
+    Errors:
+    During compile time errors occur because of syntax and semantic. The syntax error occurs because of the wrong
+    syntax of the written code. Semantic errors occur in reference to variable, function, type declarations and
+    type checking.
+*/
+int main()
+{
+}
