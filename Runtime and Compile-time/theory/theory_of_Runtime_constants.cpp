38fe904ad47def18b3b9cd8218bd262c935b2917
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-03 18:23:57 +0600

    theory of Runtime constants

---
+++ Runtime and Compile-time/theory/theory_of_Runtime_constants.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/runtime-and-compile-time-constants-in-c/
+                  https://www.javatpoint.com/compile-time-vs-runtime
+                  https://nus-cs1010.github.io/2223-s1/14-array.html
+                  https://www.learncpp.com/cpp-tutorial/compile-time-constants-constant-expressions-and-constexpr/
+                  https://www.learncpp.com/cpp-tutorial/arrays-part-i/
+                  https://www.techrepublic.com/article/from-compile-time-constants-to-runtime-constants-and-back/
+                  https://www.geeksforgeeks.org/memory-layout-of-c-program/
+                  https://www.baeldung.com/cs/runtime-vs-compile-time
+*/
+/*
+    Run-time Constant:
+    These are the constants whose respective values can only be known or computed at the time of running of source
+    code. Run time Constants are a bit slower than compile-time constants but are more flexible than compile-time
+    constants. However, once it is initialized, the value of these constants can’t be changed.
+
+    when we input in console, that is runtime constant.
+
+    like an example:
+        cout<<"Enter a number: ";
+        int y;
+        cin>>y;
+*/
+int main()
+{
+}
