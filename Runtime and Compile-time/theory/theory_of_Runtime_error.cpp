b4d1a6d44f3505cceb609c63bbd480eb8cafe519
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-03 18:24:22 +0600

    theory of Runtime error

---
+++ Runtime and Compile-time/theory/theory_of_Runtime_error.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/runtime-and-compile-time-constants-in-c/
+                  https://www.javatpoint.com/compile-time-vs-runtime
+                  https://nus-cs1010.github.io/2223-s1/14-array.html
+                  https://www.learncpp.com/cpp-tutorial/compile-time-constants-constant-expressions-and-constexpr/
+                  https://www.learncpp.com/cpp-tutorial/arrays-part-i/
+                  https://www.techrepublic.com/article/from-compile-time-constants-to-runtime-constants-and-back/
+                  https://www.geeksforgeeks.org/memory-layout-of-c-program/
+                  https://www.baeldung.com/cs/runtime-vs-compile-time
+*/
+/*
+    Runtime errors
+    The runtime errors are the errors that occur during the execution and after compilation. The examples of
+    runtime errors are division by zero, etc. These errors are not easy to detect as the compiler does not point
+    to these errors.
+*/
+/*
+    Runtime errors
+    A program’s life cycle is a runtime when the program is in execution. Following are the different types of
+    runtime errors:
+    1.Division by zero – when a number is divided by zero (0)
+    2.Dereferencing a null pointer – when a program attempts to access memory with a NULL
+    3.Running out of memory – when a computer has no memory to allocate to programs
+*/
+int main()
+{
+}
