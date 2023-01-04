022a089caebfcc474f707894bfcc69e09e73d9e4
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-04 18:19:57 +0600

    theory of memory allocation

---
+++ memory allocation/theory/theory_of_memory_allocation.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/stack-vs-heap-memory-allocation/
+                  https://www.guru99.com/stack-vs-heap.html
+                  https://www.javatpoint.com/dynamic-memory-allocation-in-cpp
+                  https://courses.engr.illinois.edu/cs225/fa2022/resources/stack-heap/
+                  https://www.learncpp.com/cpp-tutorial/the-stack-and-the-heap/
+                  https://www.softwaretestinghelp.com/stack-vs-heap/
+                  https://www.makeuseof.com/stack-and-heap-memory-allocation/
+                  https://www.javatpoint.com/stack-vs-heap
+                  https://www.geeksforgeeks.org/what-is-memory-leak-how-can-we-avoid/
+*/
+/*
+    Memory allocation:
+    Reserving or providing space to a variable is called memory allocation. For storing the data, memory allocation
+    can be done in two ways -
+
+    1.Static allocation or compile-time allocation - Static memory allocation means providing space for the variable.
+    The size and data type of the variable is known, and it remains constant throughout the program. Static allocation
+    happen in stack memory.
+    2.Dynamic allocation or run-time allocation - The allocation in which memory is allocated dynamically. In this type
+    of allocation, the exact size of the variable is not known in advance. Pointers play a major role in dynamic memory
+    allocation. Dynamic allocation happen in heap memory.
+*/
+/*
+    In C++, memory is divided into two parts -
+
+    Stack - All the variables that are declared inside any function take memory from the stack.
+    Heap - It is unused memory in the program that is generally used for dynamic memory allocation.
+*/
+int main()
+{
+}
