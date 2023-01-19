c1b27565fd55cb26dccc592912c2919be135831a
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-19 22:01:34 +0600

    example of is-sorted algorithm

---
+++ STL components/Algorithms/Algorithms/sorting/example_of_is_sorted_algorithm.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/sort-c-stl/
+                  https://www.digitalocean.com/community/tutorials/sort-in-c-plus-plus
+                  https://www.studytonight.com/cpp/stl/stl-sorting-algorithms
+*/
+int main()
+{
+    int a[] = {9,8,7,6,5,4,3,2,1};
+    int len = sizeof(a)/sizeof(int);
+
+    cout<<"a[]: ";
+    for(int i: a)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<endl;
+
+
+    cout<<"is array a is sorted or not in ascending order: "<<is_sorted(a, a+len)<<endl<<endl;
+
+
+    cout<<"-----------------------------------------------------------------------------"<<endl<<endl;
+
+    int b[] = {1,2,3,4,5,11,15};
+    int len2 = sizeof(b)/sizeof(int);
+
+    cout<<"b[]: ";
+    for(int i: b)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<endl;
+
+    cout<<"is array b is sorted or not in ascending order: "<<is_sorted(b, b+len2)<<endl<<endl;
+
+
+    cout<<"-----------------------------------------------------------------------------"<<endl<<endl;
+
+    int c[] = {15,12,9,8,6,5,0};
+    int len3 = sizeof(c)/sizeof(int);
+
+    cout<<"c[]: ";
+    for(int i: c)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl<<endl;
+
+    cout<<"is array c is sorted or not in descending order: "<<is_sorted(c, c+len3, greater<int>() )<<endl<<endl;
+
+
+    //output:
+    //    a[]: 9 8 7 6 5 4 3 2 1
+    //
+    //    is array a is sorted or not in ascending order: 0
+    //
+    //    -----------------------------------------------------------------------------
+    //
+    //    b[]: 1 2 3 4 5 11 15
+    //
+    //    is array b is sorted or not in ascending order: 1
+    //
+    //    -----------------------------------------------------------------------------
+    //
+    //    c[]: 15 12 9 8 6 5 0
+    //
+    //    is array c is sorted or not in descending order: 1
+}
+
