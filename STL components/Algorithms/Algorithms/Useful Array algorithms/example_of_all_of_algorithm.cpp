712e1d13e10176daed1960145ec84128900ecd9d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-22 18:29:24 +0600

    example of all of algorithm

--- STL components/Algorithms/Algorithms/Useful Array algorithms/example_of_all_of_algorithm.cpp
+++ STL components/Algorithms/Algorithms/Useful Array algorithms/example_of_all_of_algorithm.cpp
@@ -5,6 +5,11 @@ using namespace std;
     to know more: https://www.geeksforgeeks.org/useful-array-algorithms-in-c-stl/
                   https://www.studytonight.com/cpp/stl/stl-modifying-algorithms
 */
+/*
+    all_of() This function operates on whole range of array elements and can save time to run a loop to
+    check each elements one by one. It checks for a given property on every element and returns true when
+    each element in range satisfies specified property, else returns false.
+*/
 int main()
 {
     // Initializing array
