aa5b6a41073bf0b30a84ed50e27cbd82e841a122
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-23 18:09:19 +0600

    theory of valarray class

---
+++ STL components/Algorithms/numeric/theory/theory_of_valarray_class.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.geeksforgeeks.org/std-valarray-class-c/
+*/
+/*
+    C++98 introduced a special container called valarray to hold and provide mathematical operations on
+    arrays efficiently.
+
+    It supports element-wise mathematical operations and various forms of generalized subscript operators,
+    slicing and indirect access. As compare to vectors, valarrays are efficient in certain mathematical operations
+    than vectors also.
+*/
+/*
+    Public member functions in valarray class :
+    1. apply()
+    2. sum()
+    3. min()
+    4. max()
+    5. shift()
+    6. cshift()
+    7. swap()
+*/
+int main()
+{
+}
