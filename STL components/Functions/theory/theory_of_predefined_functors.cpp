47b9fa37728ca3bdf61c78d135b618e89ee2f900
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-24 18:53:27 +0600

    theory of predefined functors

--- STL components/Functions/theory/theory_of_predefined_functors.cpp
+++ STL components/Functions/theory/theory_of_predefined_functors.cpp
@@ -10,6 +10,16 @@ using namespace std;
                   https://www.studytonight.com/cpp/stl/
 */
 /*
+    Please note that the title is Functors (Not Functions)!!
+
+    Consider a function that takes only one argument. However, while calling this function
+    we have a lot more information that we would like to pass to this function, but we cannot
+    as it accepts only one parameter. What can be done?
+
+    One obvious answer might be global variables. However, good coding practices do not advocate
+    the use of global variables and say they must be used only when there is no other alternative.
+*/
+/*
     A function object, or functor, is any type that implements operator(). This operator is referred to as
     the call operator or sometimes the application operator. The C++ Standard Library uses function objects
     primarily as sorting criteria for containers and in algorithms.
