b94b88b735d6eb933be65bd362038a307fbe3c92
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-24 18:52:22 +0600

    theory of predefined functors

---
+++ STL components/Functions/theory/theory_of_predefined_functors.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/functors-in-cpp/
+                  https://www.programiz.com/cpp-programming/functors
+                  https://www.go4expert.com/articles/cpp-stl-functors-t34696/
+                  https://www.technical-recipes.com/2011/using-function-objects-functors-c/
+                  https://www.quantstart.com/articles/Function-Objects-Functors-in-C-Part-1/
+                  https://learn.microsoft.com/en-us/cpp/standard-library/function-objects-in-the-stl?view=msvc-170
+                  https://www.studytonight.com/cpp/stl/
+*/
+/*
+    A function object, or functor, is any type that implements operator(). This operator is referred to as
+    the call operator or sometimes the application operator. The C++ Standard Library uses function objects
+    primarily as sorting criteria for containers and in algorithms.
+
+    Function objects provide two main advantages over a straight function call. The first is that a function
+    object can contain state. The second is that a function object is a type and therefore can be used as
+    a template parameter.
+*/
+/*
+    We can use predefined functors provided by the standard library by including the functional header file:
+    #include <functional>
+
+    C++ provides the following library functors for arithmetic, relational, and logical operations.
+
+    1. Arithmetic Functors
+        Functors	Description
+        ------------------------
+        plus	    returns the sum of two parameters
+        minus	    returns the difference of two parameters
+        multiplies	returns the product of two parameters
+        divides	    returns the result after dividing two parameters
+        modulus	    returns the remainder after dividing two parameters
+        negate	    returns the negated value of a parameter
+
+    2. Relational Functors
+        Functors	    Description
+        ----------------------------
+        equal_to	    returns true if the two parameters are equal
+        not_equal_to	returns true if the two parameters are not equal
+        greater	        returns true if the first parameter is greater than the second
+        greater_equal	returns true if the first parameter is greater than or equal to the second
+        less	        returns true if the first parameter is less than the second
+        less_equal	    returns true if the first parameter is less than or equal to the second
+
+    3. Logical Functors
+        Functors	    Description
+        ------------------------------
+        logical_and	    returns the result of Logical AND operation of two booleans
+        logical_or	    returns the result of Logical OR operation of two booleans
+        logical_not	    returns the result of Logical NOT operation of a boolean
+
+    4. Bitwise Functors
+        Functors	Description
+        -------------------------------
+        bit_and	    returns the result of Bitwise AND operation of two parameters
+        bit_or	    returns the result of Bitwise OR operation of two parameters
+        bit_xor	    returns the result of Bitwise XOR operation of two parameters
+*/
+int main()
+{
+}
