07d0ab4bec1fdec7b1a27d18bd734134babaf499
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-01-30 16:50:38 +0600

    logical and arithmetic functor

---
+++ STL components/Functions/example/predefined functor/Logical Functors/logical_and_arithmetic_functor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<functional>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/functors-in-cpp/
+                  https://www.programiz.com/cpp-programming/functors
+                  https://www.go4expert.com/articles/cpp-stl-functors-t34696/
+                  https://www.technical-recipes.com/2011/using-function-objects-functors-c/
+                  https://www.quantstart.com/articles/Function-Objects-Functors-in-C-Part-1/
+                  https://learn.microsoft.com/en-us/cpp/standard-library/function-objects-in-the-stl?view=msvc-170
+*/
+int main()
+{
+    int x = 0, y = 0;
+    logical_and<int> la1; // Adds two int objects
+    logical_and<> la2;// default type is void, so template specialization used
+    logical_and<void> la3; // template specialization used
+
+    cout<<la1(x, y)<<endl;
+    cout<<la2(x+1, y+2)<<endl;
+    cout<<la3(x, y)<<endl; // same as logical_and<void> ()(x, y)
+
+    cout<<logical_and<int>()(15,10)<<endl; // Explicit type as the template argument
+    cout<<logical_and<void>()(3, 4)<<endl; // template specialization used, type is auto deduced from the arguments
+    cout<<logical_and<void>()(true, false)<<endl;
+    cout<<logical_and<void>()(true, true)<<endl;
+    cout<<logical_and<void>()(false, false)<<endl;
+
+    // output:
+    //    0
+    //    1
+    //    0
+    //    1
+    //    1
+    //    0
+    //    1
+    //    0
+}
+
+
