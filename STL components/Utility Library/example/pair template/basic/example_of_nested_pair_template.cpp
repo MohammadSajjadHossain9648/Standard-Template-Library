fa680d688a29069e27c8462af90140abdc877a47
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-06 20:46:14 +0600

    example of nested pair template

--- STL components/Utility Library/example/pair template/basic/example_of_nested_pair_template.cpp
+++ STL components/Utility Library/example/pair template/basic/example_of_nested_pair_template.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 #include<utility>
 using namespace std;
-
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.geeksforgeeks.org/pair-in-cpp-stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-pair
+                  https://www.scaler.com/topics/cpp/pair-in-cpp/
+                  https://cplusplus.com/reference/utility/pair/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.studytonight.com/cpp/stl/stl-pair-template
+                  https://www.geeksforgeeks.org/tuples-in-c/
+                  https://cplusplus.com/reference/tuple/tuple/
+                  https://www.softwaretestinghelp.com/strings-pair-tuples-in-stl/
+*/
 int main()
 {
     pair < pair < int, int >, int > p1;
