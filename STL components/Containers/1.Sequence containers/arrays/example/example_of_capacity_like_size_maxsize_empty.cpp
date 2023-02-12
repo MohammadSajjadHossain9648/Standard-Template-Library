ead605129f02a9f68010e8fb55facc0429c73866
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-12 21:56:23 +0600

    example of capacity like size maxsize empty

--- STL components/Containers/1.Sequence containers/arrays/example/example_of_capacity_like_size_maxsize_empty.cpp
+++ STL components/Containers/1.Sequence containers/arrays/example/example_of_capacity_like_size_maxsize_empty.cpp
@@ -7,7 +7,12 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-array
 */
-
+/*
+    Capacity:
+    1.size() – Returns the number of elements in the array.
+    2.max_size() – Returns the maximum number of elements that the array can hold.
+    3.empty() – Returns whether the array is empty.
+*/
 int main()
 {
     array <int, 5> v = {10,20,30,40,50};
