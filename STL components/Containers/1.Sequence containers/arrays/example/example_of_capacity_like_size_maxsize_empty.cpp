8ccda4303068a08465341a40ac4f2d42a3827707
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-12 21:45:44 +0600

    example of capacity like size maxsize empty

---
+++ STL components/Containers/1.Sequence containers/arrays/example/example_of_capacity_like_size_maxsize_empty.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<array>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-array
+*/
+
+int main()
+{
+    array <int, 5> v = {10,20,30,40,50};
+
+    cout<<"array <int> v = ";
+    for(int i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Size : "<<v.size();
+    cout<<"\nMax_Size : "<<v.max_size();
+
+
+    // checks if the array is empty or not
+    if (v.empty() == false)
+        cout<<"\narray is not empty";
+    else
+        cout<<"\narray is empty";
+
+
+    return 0;
+
+    //output:
+    //    array <int> v = 10 20 30 40 50
+    //    Size : 5
+    //    Max_Size : 5
+    //    array is not empty
+}
