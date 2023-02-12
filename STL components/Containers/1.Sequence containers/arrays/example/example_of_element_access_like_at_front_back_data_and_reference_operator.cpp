5788155c70e17eace4e59e9ecb423fdc0921c0d7
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-12 21:55:35 +0600

    example of element access like at front back etc

--- STL components/Containers/1.Sequence containers/arrays/example/example_of_element_access_like_at_front_back_data_and_reference_operator.cpp
+++ STL components/Containers/1.Sequence containers/arrays/example/example_of_element_access_like_at_front_back_data_and_reference_operator.cpp
@@ -7,7 +7,14 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-array
 */
-
+/*
+    Element access:
+    1.reference operator [index_no] – Returns a reference to the element at position ‘index_no’ in the array
+    2.at(index_no) – Returns a reference to the element at position ‘index_no’ in the array
+    3.front() – Returns a reference to the first element in the array
+    4.back() – Returns a reference to the last element in the array
+    5.data() – Returns a direct pointer to the memory array used internally by the array to store its owned elements.
+*/
 int main()
 {
     array <int, 5> v = {10,20,30,40,50};
