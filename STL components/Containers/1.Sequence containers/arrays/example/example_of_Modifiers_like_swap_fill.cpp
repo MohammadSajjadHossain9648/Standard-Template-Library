dee6a87054a0b114bb62f909166359be3fa5aef3
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-13 19:42:55 +0600

    example of Modifiers like swap fill

---
+++ STL components/Containers/1.Sequence containers/arrays/example/example_of_Modifiers_like_swap_fill.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<array>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-array
+*/
+int main()
+{
+    // Assign array
+    array<int, 5> v;
+
+    // Filling array with 0
+    v.fill(5);
+
+    cout<<"The array elements are: ";
+    for (int i = 0; i < v.size(); i++)
+        cout<<v[i]<<" ";
+
+
+
+    // two array to perform swap
+    array<int, 5> v1 = {1,2,3,4,5};
+    array<int, 5> v2 = {6,7,8,9,10};
+
+    cout<<"\n\nv1: ";
+    for (int i = 0; i < v1.size(); i++)
+        cout<<v1[i]<<" ";
+
+    cout<<"\nv2: ";
+    for (int i = 0; i < v2.size(); i++)
+        cout<<v2[i]<<" ";
+
+    // Swaps v1 and v2
+    v1.swap(v2);
+
+    cout<<"\nAfter Swap \nv1: ";
+    for (int i = 0; i < v1.size(); i++)
+        cout<<v1[i]<<" ";
+
+    cout<<"\nv2: ";
+    for (int i = 0; i < v2.size(); i++)
+        cout<<v2[i]<<" ";
+
+
+    //output:
+    //    The array elements are: 5 5 5 5 5
+    //
+    //    v1: 1 2 3 4 5
+    //    v2: 6 7 8 9 10
+    //    After Swap
+    //    v1: 6 7 8 9 10
+    //    v2: 1 2 3 4 5
+}
