007c7bb2f45bc1433b617c2040c7557273d1f67c
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-14 19:45:42 +0600

    example of capacity like size maxsize resize etc

---
+++ STL components/Containers/1.Sequence containers/deque/example/example_of_capacity_like_size_maxsize_resize_empty_reserve_and_shrink_to_fit.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-deque
+                  https://www.geeksforgeeks.org/deque-cpp-stl/
+*/
+
+int main()
+{
+    deque <int> v = {10,20,30,40,50};
+
+    cout<<"deque <int> v = ";
+    for(int i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Size : "<<v.size();
+    cout<<"\nMax_Size : "<<v.max_size();
+
+    // resizes the deque size to 4
+    v.resize(4);
+
+    // prints the deque size after resize()
+    cout<<"\nSize : "<<v.size();
+
+    // checks if the deque is empty or not
+    if (v.empty() == false)
+        cout<<"\ndeque is not empty";
+    else
+        cout<<"\ndeque is empty";
+
+    // Shrinks the deque
+    v.shrink_to_fit();
+    cout<<"\ndeque elements are: ";
+    for (auto it = v.begin(); it != v.end(); it++)
+        cout<<*it<<" ";
+
+    return 0;
+
+    //output:
+    //    deque <int> v = 10 20 30 40 50
+    //    Size : 5
+    //    Max_Size : 2305843009213693951
+    //    Size : 4
+    //    deque is not empty
+    //    deque elements are: 10 20 30 40
+}
