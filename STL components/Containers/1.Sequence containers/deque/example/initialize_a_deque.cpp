ed28b3b8b368990b76b9811c3d3374e427ed7eca
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-15 18:41:35 +0600

    initialize a deque

--- STL components/Containers/1.Sequence containers/deque/example/initialize_a_deque.cpp
+++ STL components/Containers/1.Sequence containers/deque/example/initialize_a_deque.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 #include<deque>
 using namespace std;
-
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-deque
+                  https://www.geeksforgeeks.org/deque-cpp-stl/
+                  https://iq.opengenus.org/initialize-deque-in-cpp-stl/
+*/
 int main()
 {
 
