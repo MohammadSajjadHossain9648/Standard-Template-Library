3789c14214932f76de6eeeac3123e5a6a8a33ea7
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-16 19:14:53 +0600

    print deque using begin, end, rbegin etc

--- STL components/Containers/1.Sequence containers/deque/example/print_deque_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
+++ STL components/Containers/1.Sequence containers/deque/example/print_deque_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
@@ -8,6 +8,21 @@ using namespace std;
                   https://www.studytonight.com/cpp/stl/stl-container-deque
                   https://www.geeksforgeeks.org/deque-cpp-stl/
 */
+/*
+    1.begin() – Returns an iterator pointing to the first element in the deque
+    2.end() – Returns an iterator pointing to the theoretical element that follows the last element in the deque
+    3.rbegin() – Returns a reverse iterator pointing to the last element in the deque (reverse beginning).
+    It moves from last to first element
+    4.rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in
+    the deque (considered as reverse end)
+    5.cbegin() – Returns a constant iterator pointing to the first element in the deque.
+    6.cend() – Returns a constant iterator pointing to the theoretical element that follows the last element
+    in the deque.
+    7.crbegin() – Returns a constant reverse iterator pointing to the last element in the deque (reverse beginning).
+    It moves from last to first element
+    8.crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element
+    in the deque (considered as reverse end)
+*/
 int main()
 {
     deque <int> v = {10,20,30,40,50};
