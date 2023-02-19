3c3e8192f40c85ca9b3b7438a43b5796f824111d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-19 20:03:35 +0600

    example of element access like front

---
+++ STL components/Containers/1.Sequence containers/forward_list/example/example_of_element_access_like_front.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<forward_list>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-forward_list
+*/
+int main()
+{
+    forward_list <int> v = {10,20,30,40,50};
+
+    cout<<"forward_list <int> v = ";
+    for(int i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+    cout<<"\nfront() : v.front() = "<<v.front();
+
+
+    return 0;
+
+    //output:
+    //    forward_list <int> v = 10 20 30 40 50
+    //    front() : v.front() = 10
+
+}
