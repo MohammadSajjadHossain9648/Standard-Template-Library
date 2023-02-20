6141435ffb3b8b3e294ad43383b2fa1e95aca981
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-20 20:33:15 +0600

    print forward list using begin, end, cbegin, cend

---
+++ STL components/Containers/1.Sequence containers/forward_list/example/print_forward_list_using_begin_end_cbegin_cend.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<forward_list>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-forward_list
+*/
+int main()
+{
+    forward_list <int> v = {10,20,30,40,50};
+
+    cout<<"forward_list <int> v = ";
+    for(int i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Output of begin and end: ";
+    for (auto i = v.begin(); i != v.end(); ++i)
+        cout<<*i<<" ";
+
+    cout<<"\nOutput of cbegin and cend: ";
+    for (auto i = v.cbegin(); i != v.cend(); ++i)
+        cout<<*i<<" ";
+
+    return 0;
+
+    //output:
+    //    forward_list <int> v = 10 20 30 40 50
+    //    Output of begin and end: 10 20 30 40 50
+    //    Output of cbegin and cend: 10 20 30 40 50
+
+}
