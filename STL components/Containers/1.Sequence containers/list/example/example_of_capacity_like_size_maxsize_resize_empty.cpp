2cddb89707409d775b6cd84082b972eb36004b53
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-20 20:40:30 +0600

    example of capacity like size, maxsize, resize etc

---
+++ STL components/Containers/1.Sequence containers/list/example/example_of_capacity_like_size_maxsize_resize_empty.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<list>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-list
+*/
+int main()
+{
+    list <int> v = {10,20,30,40,50};
+
+    cout<<"list <int> v = ";
+    for(int i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Size : "<<v.size();
+    cout<<"\nMax_Size : "<<v.max_size();
+
+    // resizes the list size to 4
+    v.resize(4);
+
+    // prints the list size after resize()
+    cout<<"\nSize : "<<v.size();
+
+    // checks if the list is empty or not
+    if (v.empty() == false)
+        cout<<"\nlist is not empty";
+    else
+        cout<<"\nlist is empty";
+
+
+    return 0;
+
+    //output:
+    //    list <int> v = 10 20 30 40 50
+    //    Size : 5
+    //    Max_Size : 2305843009213693951
+    //    Size : 4
+    //    list is not empty
+}
