3f1c3f1ab0037555f4f76df3132c8eba88b5f3c9
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-27 19:48:32 +0600

    print vector using begin, end, rbegin, rend etc

--- STL components/Containers/1.Sequence containers/vector/example/print_vector_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
+++ STL components/Containers/1.Sequence containers/vector/example/print_vector_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
@@ -7,6 +7,21 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-vector
 */
+/*
+    1.begin() – Returns an iterator pointing to the first element in the vector
+    2.end() – Returns an iterator pointing to the theoretical element that follows the last element in the vector
+    3.rbegin() – Returns a reverse iterator pointing to the last element in the vector (reverse beginning).
+    It moves from last to first element
+    4.rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in
+    the vector (considered as reverse end)
+    5.cbegin() – Returns a constant iterator pointing to the first element in the vector.
+    6.cend() – Returns a constant iterator pointing to the theoretical element that follows the last element
+    in the vector.
+    7.crbegin() – Returns a constant reverse iterator pointing to the last element in the vector (reverse beginning).
+    It moves from last to first element
+    8.crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element
+    in the vector (considered as reverse end)
+*/
 int main()
 {
     vector <int> v = {10,20,30,40,50};
