25b211da3480e6d8507234722ee30959b7513253
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-02-27 19:47:57 +0600

    print vector using begin, end, rbegin, rend etc

---
+++ STL components/Containers/1.Sequence containers/vector/example/print_vector_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-vector
+*/
+int main()
+{
+    vector <int> v = {10,20,30,40,50};
+
+    cout<<"vector <int, 5> v = ";
+    for(auto i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Output of begin and end: ";
+    for (auto i = v.begin(); i != v.end(); ++i)
+        cout<<*i<<" ";
+
+    cout<<"\nOutput of cbegin and cend: ";
+    for (auto i = v.cbegin(); i != v.cend(); ++i)
+        cout<<*i<<" ";
+
+//reverse print the dynamic array or vector
+    cout<<"\nOutput of rbegin and rend: ";
+    for (auto ir = v.rbegin(); ir != v.rend(); ++ir)
+        cout<<*ir<<" ";
+
+    cout<<"\nOutput of crbegin and crend : ";
+    for (auto ir = v.crbegin(); ir != v.crend(); ++ir)
+        cout<<*ir<<" ";
+
+    return 0;
+
+    //output:
+    //    vector <int, 5> v = 10 20 30 40 50
+    //    Output of begin and end: 10 20 30 40 50
+    //    Output of cbegin and cend: 10 20 30 40 50
+    //    Output of rbegin and rend: 50 40 30 20 10
+    //    Output of crbegin and crend : 50 40 30 20 10
+}
