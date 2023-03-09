a7c5dbcc80cc8a31f6839c9701ccfca1b306880c
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-03-09 19:18:20 +0600

    example of capacity like size, maxsize, empty etc

--- STL components/Containers/3.Associative containers/map/example/example_of_capacity_like_size_maxsize_empty_etc.cpp
+++ STL components/Containers/3.Associative containers/map/example/example_of_capacity_like_size_maxsize_empty_etc.cpp
@@ -12,6 +12,20 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-map
 */
+/*
+    Capacity:
+    1.size() – Returns the number of elements in the map.
+    2.max_size() – Returns the maximum number of elements that the map can hold.
+    3.empty() – Returns whether the map is empty.
+    4.find() - Returns an iterator to the element ‘g’ in the map if found, else returns the iterator to end.
+    5.count() - Returns 1 or 0 based on whether the element ‘g’ is present in the map or not.
+    6.lower_bound(const g) - Returns an iterator to the first element that is equivalent to ‘g’ or definitely
+    will not go before the element ‘g’ in the map.
+    7.upper_bound(const g) - Returns an iterator to the first element that will go after the element ‘g’ in the map.
+    8.equal_range(const g) - The function returns an iterator of pairs. (key_comp). The pair refers to the range
+    that includes all the elements in the container which have a key equivalent to k. which indiactes results of both
+    the lower_bound and the upper_bound of g.
+*/
 int main()
 {
     map <int, string> v = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
