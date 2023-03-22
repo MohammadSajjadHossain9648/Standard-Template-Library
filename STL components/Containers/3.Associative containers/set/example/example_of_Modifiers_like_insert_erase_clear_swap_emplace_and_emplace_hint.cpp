75a3a1c9aad2b7595a8742264f507e07bd25289c
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-03-22 20:20:29 +0600

    example of Modifiers like insert, erase, clear etc

--- STL components/Containers/3.Associative containers/set/example/example_of_Modifiers_like_insert_erase_clear_swap_emplace_and_emplace_hint.cpp
+++ STL components/Containers/3.Associative containers/set/example/example_of_Modifiers_like_insert_erase_clear_swap_emplace_and_emplace_hint.cpp
@@ -11,6 +11,21 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-set
 */
+/*
+    Modifiers:
+    1.insert() - Insert element in the set.
+    2.insert(iterator position, const g) - Adds a new element ‘g’ at the position pointed by the iterator.
+    3.erase(const g) - Removes the value ‘g’ from the set.
+    4.erase(iterator position) - Removes the element at the position pointed by the iterator.
+    5.clear() - Removes all the elements from the set.
+    6.emplace() - This function is used to insert a new element into the set container, only if the element
+    to be inserted is unique and does not already exist in the set.
+    7.emplace_hint() - Returns an iterator pointing to the position where the insertion is done. If the element
+    passed in the parameter already exists, then it returns an iterator pointing to the position where the existing
+    element is.
+    8.swap() - This function is used to exchange the contents of two sets but the sets must be of the same type,
+    although sizes may differ.
+*/
 int main()
 {
     set<int> v = {10,50,40};
