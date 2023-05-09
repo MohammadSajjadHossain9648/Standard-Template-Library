732efece45860129f2532e8aee20db7317937a7b
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-09 21:27:48 +0600

    print multiset using begin, end etc

--- STL components/Containers/3.Associative containers/multiset/example/print_multiset_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
+++ STL components/Containers/3.Associative containers/multiset/example/print_multiset_using_begin_end_rbegin_rend_cbegin_cend_crbegin_crend.cpp
@@ -2,18 +2,28 @@
 #include<set>
 using namespace std;
 /*
-    1.begin() – Returns an iterator pointing to the first element in the multiset
-    2.end() – Returns an iterator pointing to the theoretical element that follows the last element in the multiset
-    3.rbegin() – Returns a reverse iterator pointing to the last element in the multiset (reverse beginning).
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.mygreatlearning.com/blog/multiset-in-cpp/
+                  https://www.javatpoint.com/cpp-multiset
+                  https://www.scaler.com/topics/cpp/multiset-in-cpp/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-multiset
+*/
+/*
+    1.begin() â€“ Returns an iterator pointing to the first element in the multiset
+    2.end() â€“ Returns an iterator pointing to the theoretical element that follows the last element in the multiset
+    3.rbegin() â€“ Returns a reverse iterator pointing to the last element in the multiset (reverse beginning).
     It moves from last to first element
-    4.rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in
+    4.rend() â€“ Returns a reverse iterator pointing to the theoretical element preceding the first element in
     the multiset (considered as reverse end)
-    5.cbegin() – Returns a constant iterator pointing to the first element in the multiset.
-    6.cend() – Returns a constant iterator pointing to the theoretical element that follows the last element
+    5.cbegin() â€“ Returns a constant iterator pointing to the first element in the multiset.
+    6.cend() â€“ Returns a constant iterator pointing to the theoretical element that follows the last element
     in the multiset.
-    7.crbegin() – Returns a constant reverse iterator pointing to the last element in the multiset (reverse beginning).
+    7.crbegin() â€“ Returns a constant reverse iterator pointing to the last element in the multiset (reverse beginning).
     It moves from last to first element
-    8.crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element
+    8.crend() â€“ Returns a constant reverse iterator pointing to the theoretical element preceding the first element
     in the multiset (considered as reverse end)
 */
 int main()
