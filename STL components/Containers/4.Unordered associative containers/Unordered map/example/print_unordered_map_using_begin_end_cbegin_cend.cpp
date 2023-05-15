1b50ae5ebe48c202178374644e0404a85ce6fc52
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-15 19:39:59 +0600

    print unordered map using begin, end, cbegin, cend

--- STL components/Containers/4.Unordered associative containers/Unordered map/example/print_unordered_map_using_begin_end_cbegin_cend.cpp
+++ STL components/Containers/4.Unordered associative containers/Unordered map/example/print_unordered_map_using_begin_end_cbegin_cend.cpp
@@ -11,6 +11,14 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-unordered_map
 */
+/*
+    Iterators:
+    1.begin() – Returns an iterator pointing to the first element in the unordered_map
+    2.end() – Returns an iterator pointing to the theoretical element that follows the last element in the unordered_map
+    3.cbegin() – Returns a constant iterator pointing to the first element in the unordered_map.
+    4.cend() – Returns a constant iterator pointing to the theoretical element that follows the last element
+    in the unordered_map.
+*/
 int main()
 {
     unordered_map<int, string> v = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
