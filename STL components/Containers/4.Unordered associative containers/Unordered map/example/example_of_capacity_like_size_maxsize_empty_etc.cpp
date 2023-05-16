78989c734a3280d4a4192f98028fa99504d14729
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-16 22:06:56 +0600

    example of capacity like size, maxsize, empty etc

--- STL components/Containers/4.Unordered associative containers/Unordered map/example/example_of_capacity_like_size_maxsize_empty_etc.cpp
+++ STL components/Containers/4.Unordered associative containers/Unordered map/example/example_of_capacity_like_size_maxsize_empty_etc.cpp
@@ -12,6 +12,17 @@ using namespace std;
                   https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
                   https://www.studytonight.com/cpp/stl/stl-container-unordered_map
 */
+/*
+    Capacity:
+    1.size() – Returns the number of elements in the unordered_map.
+    2.max_size() – Returns the maximum number of elements that the unordered_map can hold.
+    3.empty() – Returns whether the unordered_map is empty.
+    4.find() - Returns an iterator to the element ‘g’ in the unordered_map if found, else returns the iterator to end.
+    5.count() - Returns 1 or 0 based on whether the element ‘g’ is present in the unordered_map or not.
+    6.equal_range(const g) - The function returns an iterator of pairs. (key_comp). The pair refers to the range
+    that includes all the elements in the container which have a key equivalent to k. which indiactes results of both
+    the lower_bound and the upper_bound of g.
+*/
 int main()
 {
     unordered_map<int, string> v = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
