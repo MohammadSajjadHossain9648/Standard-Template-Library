a7560b6407db590cbcdb8c1bb317e08de250a1d2
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-16 22:04:37 +0600

    example of capacity like size, maxsize, empty etc

---
+++ STL components/Containers/4.Unordered associative containers/Unordered map/example/example_of_capacity_like_size_maxsize_empty_etc.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.mygreatlearning.com/blog/unordered_map-in-cpp/
+                  https://www.javatpoint.com/cpp-unordered_map
+                  https://www.scaler.com/topics/cpp/unordered_map-in-cpp/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-unordered_map
+*/
+int main()
+{
+    unordered_map<int, string> v = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    cout<<"\nmap<int, string> v\nid\tname\n";
+    for(auto i: v)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    cout<<"Size : "<<v.size();
+    cout<<"\nMax Size : "<<v.max_size();
+
+
+//3.empty
+    // checks if the unordered_map is empty or not
+    if (v.empty() == false)
+        cout<<"\nunordered_map is not empty\n";
+    else
+        cout<<"\nunordered_map is empty\n";
+
+
+//4.find
+    //example 1: find the value 3 is present or not
+    auto pos = v.find(3); //if found the value, pos store the value. else pos = v.end() which is null means nothing found.
+
+    if(pos != v.end())
+    {
+        cout<<"\nfound and the searching value is "<<pos->first<<" and name is "<<pos->second<<endl;
+    }
+    else
+    {
+        cout<<"\nsearching value didnot found in the unordered_map.\n";
+    }
+
+
+
+    //example 2: find the value 5 and delete it from unordered_map
+    v.erase(v.find(5));
+
+    /* can be write above line
+        auto pos = v.find(5);
+        v.erase(pos);
+    */
+
+    cout<<"\nafter delete 5 from unordered_map\n";
+    cout<<"\nmap<int, string> v\nid\tname\n";
+    for(auto i: v)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    /* never use it unordered_multimap cause its didnot follow any order
+    //example 3: print all values after 2.
+    v = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    auto pos1 = v.find(5);
+    cout<<"\nprint all values after 5 \nunordered_map<int, string> v = ";
+    for(auto i=pos1; i!=v.end(); i++)
+    {
+        cout<<i->first<<"\t"<<i->second<<endl;
+    }
+    cout<<endl;
+    */
+
+
+
+//5.count  -> same as find
+    //example 1: find the value 3 is present or not by counting,
+    //if value present, count = 1(true). otherwise count = 0(false) means not present
+    if(v.count(3)) //can be write, if(v.count(3) == 1) or if(v.count(3) > 0)
+    {
+        cout<<"\nfound by count and the searching value is 3";
+    }
+    else
+    {
+        cout<<"\nsearching value didnot found in the unordered_map.";
+    }
+
+
+    //example 2: check all values 1...10 is present or not by counting, if count = 1, value present. of count = 0, not present
+    unordered_map<int, string> v1 = {{2,"sakib"},{4,"asif"},{6,"liton"},{8,"tamim"},{10,"afif"}};
+
+    for(auto i=1; i<=10; i++)
+    {
+        if(v1.count(i)) //can be write, if(v.count(i) == 1) or if(v.count(i) > 0)
+        {
+           cout<<"\nsearching value "<<i<<" found by count";
+        }
+        else
+        {
+           cout<<"\nsearching value "<<i<<" didnot found in the unordered_map.";
+        }
+    }
+    cout<<endl;
+
+
+//6.equal_range which gives the result of both the lower_bound and the upper_bound of any value.
+    unordered_map<int, string> v3 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+    cout<<"\nmap<int, string> v3\nid\tname\n";
+    for(auto i: v3)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+    auto it5 = v3.equal_range(3);
+
+    cout<<"\nthe lower bound value of 3 is "<<it5.first->first;
+    cout<<"\nthe upper bound value of 3 is "<<it5.second->first;
+
+
+    return 0;
+
+    //output:
+    //
+    //    map<int, string> v
+    //    id      name
+    //    4       afif
+    //    3       tamim
+    //    5       liton
+    //    1       asif
+    //    2       sakib
+    //
+    //    Size : 5
+    //    Max Size : 192153584101141162
+    //    unordered_map is not empty
+    //
+    //    found and the searching value is 3 and name is tamim
+    //
+    //    after delete 5 from unordered_map
+    //
+    //    map<int, string> v
+    //    id      name
+    //    4       afif
+    //    3       tamim
+    //    1       asif
+    //    2       sakib
+    //
+    //
+    //
+    //    found by count and the searching value is 3
+    //    searching value 1 didnot found in the unordered_map.
+    //    searching value 2 found by count
+    //    searching value 3 didnot found in the unordered_map.
+    //    searching value 4 found by count
+    //    searching value 5 didnot found in the unordered_map.
+    //    searching value 6 found by count
+    //    searching value 7 didnot found in the unordered_map.
+    //    searching value 8 found by count
+    //    searching value 9 didnot found in the unordered_map.
+    //    searching value 10 found by count
+    //
+    //    map<int, string> v3
+    //    id      name
+    //    4       afif
+    //    3       tamim
+    //    5       liton
+    //    1       asif
+    //    2       sakib
+    //
+    //
+    //    the lower bound value of 3 is 3
+    //    the upper bound value of 3 is 5
+}
