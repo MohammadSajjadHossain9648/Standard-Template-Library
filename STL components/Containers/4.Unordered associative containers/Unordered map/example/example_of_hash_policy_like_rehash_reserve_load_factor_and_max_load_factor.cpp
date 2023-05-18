829cc1db6718914c2d9b5dc245f9f378fe428970
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-18 19:01:09 +0600

    example of hash policy like rehash, reserve etc

---
+++ STL components/Containers/4.Unordered associative containers/Unordered map/example/example_of_hash_policy_like_rehash_reserve_load_factor_and_max_load_factor.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+                  https://www.studytonight.com/cpp/stl/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.mygreatlearning.com/blog/unordered_map-in-cpp/
+                  https://www.javatpoint.com/cpp-unordered_map
+                  https://www.scaler.com/topics/cpp/unordered_map-in-cpp/
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/cpp-standard-template-library#:~:text=C%2B%2B%20STL%20(standard%20template%20library)%20is%20a%20software%20library%20for,%2C%20algorithms%2C%20and%20function%20objects.
+                  https://www.studytonight.com/cpp/stl/stl-container-unordered_map
+*/
+int main()
+{
+//rehash & reserve
+    unordered_map<int, string> v;
+    v.reserve(0);  // same as v.rehash(5);
+
+    v.insert(make_pair(5,"liton"));
+    v.insert(make_pair(4,"afif"));
+    v.insert(make_pair(1,"asif"));
+    v.insert(make_pair(2,"sakib"));
+    v.insert(make_pair(3,"tamim"));
+
+    cout<<"\nmap<int, string> v\nid\tname\n";
+    for(auto i: v)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+    cout<<"\nThe samplemap container has "<<v.bucket_count()<<" number of buckets\n";
+
+
+//load_factor and max_load_factor
+    unordered_map<int, string> sample = {{2,"sakib"},{1,"asif"}};
+
+    cout<<"\nThe size is: "<<sample.size();
+    cout<<"\nThe bucket_count is: "<<sample.bucket_count();
+    cout<<"\nThe load_factor is: "<<sample.load_factor();
+    cout<<"\nThe max_load_factor is: "<<sample.max_load_factor();
+
+    sample.insert(make_pair(3,"tamim"));
+    sample.insert(make_pair(4,"afif"));
+
+    cout<<"\n\nThe size is: "<<sample.size();
+    cout<<"\nThe bucket_count is: "<<sample.bucket_count();
+    cout<<"\nThe load_factor is: "<<sample.load_factor();
+    cout<<"\nThe max_load_factor is: "<<sample.max_load_factor();
+
+    sample.insert(make_pair(5,"liton"));
+
+    cout<<"\n\nThe size is: "<<sample.size();
+    cout<<"\nThe bucket_count is: "<<sample.bucket_count();
+    cout<<"\nThe load_factor is: "<<sample.load_factor();
+    cout<<"\nThe max_load_factor is: "<<sample.max_load_factor();
+
+
+    //output:
+    //
+    //    map<int, string> v
+    //    id      name
+    //    3       tamim
+    //    2       sakib
+    //    1       asif
+    //    5       liton
+    //    4       afif
+    //
+    //
+    //    The samplemap container has 5 number of buckets
+    //
+    //    The size is: 2
+    //    The bucket_count is: 13
+    //    The load_factor is: 0.153846
+    //    The max_load_factor is: 1
+    //
+    //    The size is: 4
+    //    The bucket_count is: 13
+    //    The load_factor is: 0.307692
+    //    The max_load_factor is: 1
+    //
+    //    The size is: 5
+    //    The bucket_count is: 13
+    //    The load_factor is: 0.384615
+    //    The max_load_factor is: 1
+}
