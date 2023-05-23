c8bdf3d5dcae448fb2e2aa94773fe4eb8e85bc9d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-23 19:44:02 +0600

    example of get allocator, key eq and hash function

--- STL components/Containers/4.Unordered associative containers/Unordered multimap/example/example_of_get_allocator_key_eq_and_hash_function.cpp
+++ STL components/Containers/4.Unordered associative containers/Unordered multimap/example/example_of_get_allocator_key_eq_and_hash_function.cpp
@@ -12,6 +12,18 @@ using namespace std;
                   https://www.studytonight.com/cpp/stl/stl-container-unordered_multimap
                   https://www.geeksforgeeks.org/unordered_multimap-get_allocator-in-c-stl/
 */
+/*
+    Allocator:
+    1.get_allocator() - C++ unordered_multimap get_allocator() function is used to return the copy of allocator object which
+    helps to construct the unordered_multimap container.
+    Time Complexity: O(1).
+    2.key_eq() - returns a boolean value according to the comparison. It returns the key equivalence comparison
+    predicate used by the unordered_multimap. The key equivalence comparison is a predicate that takes two arguments and
+    returns a bool value indicating whether they are equal.
+    3.hash_function() - hash_function() is a built-in function in C++ STL which is used to get hash function.
+    This hash function is a unary function which takes asingle argument only and returns a unique value of type
+    size_t based on it.
+*/
 int main()
 {
 //Example 1: The program below shows how allocator of a unordered_multimap can be used to allocate an array of 5 elements.
