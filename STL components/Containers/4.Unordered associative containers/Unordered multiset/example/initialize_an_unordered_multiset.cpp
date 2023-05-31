52216fd8ff79431c6b8082c02358e779b55fb943
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-05-31 19:23:13 +0600

    initialize an unordered multiset

---
+++ STL components/Containers/4.Unordered associative containers/Unordered multiset/example/initialize_an_unordered_multiset.cpp
@@ -0,0 +1,132 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+/*
+    always remember: whatever you input in unordered_multiset, it always take unique values.
+*/
+int main()
+{
+
+//for taking user input in unordered_multiset
+    unordered_multiset<int> v;
+    int x, len;
+    cout<<"enter the length of unordered_multiset: ";
+    cin>>len;
+
+    cout<<"unordered_multiset<int> v : \n";
+    for(int i=0; i<len; i++)
+    {
+        cin>>x;
+        v.insert(x);
+    }
+    cout<<endl;
+
+    cout<<"unordered_multiset<int> v = ";
+    for(auto i: v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+// initialize in unordered_multiset
+    unordered_multiset <int> v1 = {10,20,50,40,30};
+    cout<<"unordered_multiset<int> v1 = ";
+    for(auto i: v1)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    //same as before v1
+    unordered_multiset<int> v2;
+    for(auto i:{1,2,5,4,3})
+    {
+        v2.insert(i);
+    }
+    cout<<endl;
+
+    cout<<"unordered_multiset<int> v2 = ";
+    for(auto i: v2)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    //or
+    unordered_multiset<int> v3;
+    v3.insert(10);
+    v3.insert(9);
+    v3.insert(8);
+    v3.insert(7);
+    v3.insert(6);
+
+    cout<<"\nunordered_multiset<int> v3 = ";
+    for(auto i: v3)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+    //or can be write same as use emplace instead of insert
+    unordered_multiset<int> v4;
+    v4.emplace(8);
+    v4.emplace(7);
+    v4.emplace(9);
+    v4.emplace(6);
+    v4.emplace(10);
+
+    cout<<"\nunordered_multiset<int> v4 = ";
+    for(auto i: v4)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+// iterator
+    // print unordered_multiset element using iteration
+    unordered_multiset<int> s = {12,43,234,65,34,54,3};
+
+    unordered_multiset<int>::iterator it;  // Creating the iterator.
+
+    cout<<"unordered_multiset<int> s = ";
+    for (it = s.begin(); it != s.end(); it++)
+    {
+    	cout<<*it<<" ";
+    }
+    cout<<endl;
+
+    // print unordered_multiset element without using iteration
+    unordered_multiset<int> s1 = {12,43,234,65,34,54,3};
+
+    cout<<"unordered_multiset<int> s1 = ";
+    for (auto i: s1)
+    {
+    	cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+
+    //output:
+    //    enter the length of unordered_multiset: 5
+    //    unordered_multiset<int> v :
+    //    10
+    //    20
+    //    30
+    //    10
+    //    40
+    //
+    //    unordered_multiset<int> v = 40 30 20 10 10
+    //    unordered_multiset<int> v1 = 30 40 50 20 10
+    //
+    //    unordered_multiset<int> v2 = 3 4 5 2 1
+    //
+    //    unordered_multiset<int> v3 = 6 7 8 9 10
+    //
+    //    unordered_multiset<int> v4 = 10 6 9 7 8
+    //    unordered_multiset<int> s = 34 65 3 234 43 54 12
+    //    unordered_multiset<int> s1 = 34 65 3 234 43 54 12
+}
