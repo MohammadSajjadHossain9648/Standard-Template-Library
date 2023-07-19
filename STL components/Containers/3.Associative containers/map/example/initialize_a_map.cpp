99c154706c483f4f895c3c904a70eb10dd35571a
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-07-19 17:22:18 +0600

    initialize a map

--- STL components/Containers/3.Associative containers/map/example/initialize_a_map.cpp
+++ STL components/Containers/3.Associative containers/map/example/initialize_a_map.cpp
@@ -4,6 +4,7 @@ using namespace std;
 /*
     **Always remember: Maps are the associative containers that store sorted key-value pair, in which each key
     is unique and it can be inserted or deleted but cannot be altered. Values associated with keys can be changed.
+    it store values in ascending order.
 */
 int main()
 {
@@ -12,10 +13,10 @@ int main()
     map<int, string> v;
     int id, len;
     string name;
-    cout<<"enter the length of id numbers: ";
+    cout<<"enter the length of map: ";
     cin>>len;
 
-    cout<<"map<int, string>  v : \n";
+    cout<<"map<int, string> v : \n";
     for(int i=0; i<len; i++)
     {
         cout<<"id: ";
@@ -40,6 +41,8 @@ int main()
     cout<<endl;
 
 
+
+
 // initialize in map
     map <int, string> v1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
@@ -49,46 +52,18 @@ int main()
         cout<<i.first<<"\t"<<i.second<<endl;
     }
     cout<<endl;
-    /* can be write istead of above v1 loop
-        cout<<"\nmap<int, string> v1\nid\tname\n";
-        for (auto i = v1.begin(); i != v1.end(); ++i)
-            cout<<i->first<<"\t"<<i->second<<endl;
-    */
 
 
-//can intialize map by using operator like[]
-     map <int, string> v4;
-     v4[1] = "asif";
-     v4[2] = "sakib";
-     v4[3] = "tamim";
-     v4[4] = "afif";
-     v4[5] = "liton";
 
-    cout<<"\nmap<int, string> v4\nid\tname\n";
-    for(auto i: v4)
-    {
-        cout<<i.first<<"\t"<<i.second<<endl;
-    }
-    cout<<endl;
+    //can intialize map by using operator like[]
+    map <int, string> v2;
+    v2[2] = "sakib";
+    v2[1] = "asif";
+    v2[5] = "liton";
+    v2[3] = "tamim";
+    v2[4] = "afif";
 
-//can print individually
-    cout<<"v1[id] = name -> v1[1] = "<<v1[1]<<endl;
-    cout<<"v1[id] = name -> v1[3] = "<<v1[3]<<endl;
-
-//can print individually using at
-    cout<<"v1[id] = name -> v1[2] = "<<v1.at(2)<<endl;
-    cout<<"v1[id] = name -> v1[4] = "<<v1.at(4)<<endl;
-
-    //same as before v1
-    map<int, string> v2;
-    v2.insert(pair<int, string>(2,"sakib")); //same as v2.insert(make_pair(2,"sakib"));
-    v2.insert(pair<int, string>(1,"asif"));
-    v2.insert(pair<int, string>(5,"liton"));
-    v2.insert(pair<int, string>(3,"tamim"));
-    v2.insert(pair<int, string>(4,"afif"));
-
-
-    cout<<"\nmap<int, string> v2\nid\tname\n";
+    cout<<"\nmap<string, int> v2\nid\tname\n";
     for(auto i: v2)
     {
         cout<<i.first<<"\t"<<i.second<<endl;
@@ -96,13 +71,13 @@ int main()
     cout<<endl;
 
 
-    //or can be write same as use emplace instead of insert
+    //or
     map<int, string> v3;
-    v3.emplace(make_pair(2,"sakib")); //we can use make_pair instead of pair<int, string>
-    v3.emplace(make_pair(1,"asif"));
-    v3.emplace(make_pair(5,"liton"));
-    v3.emplace(make_pair(3,"tamim"));
-    v3.emplace(make_pair(4,"afif"));
+    v3.insert(make_pair(2,"sakib")); //same as -> v3.insert(pair<int, string>({2,"sakib"}));
+    v3.insert(make_pair(1,"asif"));
+    v3.insert(make_pair(5,"liton"));
+    v3.insert(make_pair(3,"tamim"));
+    v3.insert(make_pair(4,"afif"));
 
 
     cout<<"\nmap<int, string> v3\nid\tname\n";
@@ -113,42 +88,114 @@ int main()
     cout<<endl;
 
 
+    //or can be write same as use emplace instead of insert
+    map<int, string> v4;
+    v4.emplace(make_pair(2,"sakib")); //same as -> v4.emplace(pair<int, string>({2,"sakib"}));
+    v4.emplace(make_pair(1,"asif"));
+    v4.emplace(make_pair(5,"liton"));
+    v4.emplace(make_pair(3,"tamim"));
+    v4.emplace(make_pair(4,"afif"));
+
+    cout<<"\nmap<int, string> v4\nid\tname\n";
+    for(auto i: v4)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    //or using pair by using one pair variable
+    map<int, string> v5;
+    pair<int, string> p;
+    p = make_pair(2,"sakib"); // never write -> p(2, "sakib");
+    v5.insert(p);
+    p = make_pair(1,"asif");
+    v5.insert(p);
+    p = make_pair(5,"liton");
+    v5.insert(p);
+    p = make_pair(3,"tamim");
+    v5.insert(p);
+    p = make_pair(4,"afif");
+    v5.insert(p);
+
+    cout<<"\nmap<int, string> v5\nid\tname\n";
+    for(auto i: v5)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    //or using pair by using many pair variables
+    map<int, string> v6;
+    pair<int, string> p1 = make_pair(2,"sakib");  //same as -> pair<int, string> p1(2,"sakib");
+    v6.insert(p1);
+    pair<int, string> p2 = make_pair(1,"asif");
+    v6.insert(p2);
+    pair<int, string> p3 = make_pair(5,"liton");
+    v6.insert(p3);
+    pair<int, string> p4 = make_pair(3,"tamim");
+    v6.insert(p4);
+    pair<int, string> p5 = make_pair(4,"afif");
+    v6.insert(p5);
+
+    cout<<"\nmap<int, string> v6\nid\tname\n";
+    for(auto i: v6)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
 
 // iterator
     // print map element using iteration
-    map<int, string>  s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+    map<int, string> s = {{2,"sakib"},{1,"asif"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
-    map<int, string> ::iterator it;  // Creating the iterator.
+    map<int, string>::iterator it;  // Creating the iterator.
 
-    cout<<"map<int, string>  s \nid\tname\n";
+    cout<<"\nmap<int, string> s\nid\tname\n";
     for (it = s.begin(); it != s.end(); it++)
     {
     	cout<<it->first<<"\t"<<it->second<<endl;
     }
     cout<<endl;
 
-    /* same as before
-        map<int, string>  s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
-        cout<<"map<int, string>  s \nid\tname\n";
-        for (auto it = s.begin(); it != s.end(); it++)
-        {
-            cout<<it->first<<"\t"<<it->second<<endl;
-        }
-        cout<<endl;
+    /* same as above
+    map<int, string> s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    map<int, string>::iterator it = s.begin();  // Creating the iterator.
+
+    cout<<"\nmap<int, string> s\nid\tname\n";
+    while(it != s.end())
+    {
+    	cout<<it->first<<"\t"<<it->second<<endl;
+    	it++;
+    }
+    cout<<endl;
     */
 
 
     // print map element without using iteration
-    map<int, string>  s1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+    map<int, string> s1 = {{2,"sakib"},{1,"asif"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
     cout<<"\nmap<int, string> s1\nid\tname\n";
-    for(auto i: s1)
+    for (auto i: s1)
     {
-        cout<<i.first<<"\t"<<i.second<<endl;
+    	cout<<i.first<<"\t"<<i.second<<endl;
     }
     cout<<endl;
 
+    /* can write also
+    map<int, string>  s1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    cout<<"map<int, string>  s1 \nid\tname\n";
+    for (auto it = s1.begin(); it != s1.end(); it++)
+    {
+        cout<<it->first<<"\t"<<it->second<<endl;
+    }
+    cout<<endl;
+    */
 
 
 
@@ -171,28 +218,30 @@ int main()
     cout<<endl;
 
 
+
     //output:
-    //    enter the length of id numbers: 5
-    //    map<int, string>  v :
+    //    enter the length of map: 5
+    //    map<int, string> v :
+    //    id: 4
+    //    name: afif
+    //    id: 3
+    //    name: tamim
+    //    id: 5
+    //    name: liton
     //    id: 1
     //    name: asif
     //    id: 2
     //    name: sakib
-    //    id: 5
-    //    name: afif
-    //    id: 3
-    //    name: liton
-    //    id: 4
-    //    name: tamim
     //
     //
     //    map<int, string> v
     //    id      name
     //    1       asif
     //    2       sakib
-    //    3       liton
-    //    4       tamim
-    //    5       afif
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
     //
     //    map<int, string> v1
     //    id      name
@@ -203,22 +252,7 @@ int main()
     //    5       liton
     //
     //
-    //
-    //    map<int, string> v4
-    //    id      name
-    //    1       asif
-    //    2       sakib
-    //    3       tamim
-    //    4       afif
-    //    5       liton
-    //
-    //    v1[id] = name -> v1[1] = asif
-    //    v1[id] = name -> v1[3] = tamim
-    //    v1[id] = name -> v1[2] = sakib
-    //    v1[id] = name -> v1[4] = afif
-    //
-    //
-    //    map<int, string> v2
+    //    map<string, int> v2
     //    id      name
     //    1       asif
     //    2       sakib
@@ -235,7 +269,35 @@ int main()
     //    4       afif
     //    5       liton
     //
-    //    map<int, string>  s
+    //
+    //    map<int, string> v4
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    map<int, string> v5
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    map<int, string> v6
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    map<int, string> s
     //    id      name
     //    1       asif
     //    2       sakib
