65038f65b5c70ef75b748d1e2de4d0a13ab9bba0
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-07-19 17:30:38 +0600

    initialize a multimap

--- STL components/Containers/3.Associative containers/multimap/example/initialize_a_multimap.cpp
+++ STL components/Containers/3.Associative containers/multimap/example/initialize_a_multimap.cpp
@@ -2,20 +2,21 @@
 #include<map>
 using namespace std;
 /*
-    **Always remember: multimaps are the associative containers that store sorted key-value pair, in which
-    each key is unique and it can be inserted or deleted but cannot be altered. Values associated with keys
-    can be changed.
+    **Always remember: multimaps are the associative containers that store sorted key-value pair, in which each key
+    is unique and it can be inserted or deleted but cannot be altered. Values associated with keys can be changed.
+    it store values in ascending order.
 */
 int main()
 {
+
 //for taking user input in multimap
     multimap<int, string> v;
     int id, len;
     string name;
-    cout<<"enter the length of id numbers: ";
+    cout<<"enter the length of multimap: ";
     cin>>len;
 
-    cout<<"multimap<int, string>  v : \n";
+    cout<<"multimap<int, string> v : \n";
     for(int i=0; i<len; i++)
     {
         cout<<"id: ";
@@ -23,6 +24,12 @@ int main()
         cout<<"name: ";
         cin>>name;
         v.insert(pair<int, string>(id, name)); //we can use make_pair instead of pair<int, string>
+
+        /*input name in ascending order of id
+        cout<<"name: ";
+        cin>>name;
+        v.insert(pair<int, string>(i+1, name)); //we can use make_pair instead of pair<int, string>
+        */
     }
     cout<<endl;
 
@@ -34,6 +41,8 @@ int main()
     cout<<endl;
 
 
+
+
 // initialize in multimap
     multimap <int, string> v1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
@@ -43,53 +52,29 @@ int main()
         cout<<i.first<<"\t"<<i.second<<endl;
     }
     cout<<endl;
-    /* can be write istead of above v1 loop
-        cout<<"\nmultimap<int, string> v1\nid\tname\n";
-        for (auto i = v1.begin(); i != v1.end(); ++i)
-            cout<<i->first<<"\t"<<i->second<<endl;
+
+
+
+    //issue -> remember that can not intialize map by using operator like[]
+    /*never use this way
+    multimap <int, string> v1;
+    v1[1] = "asif";
+    v1[2] = "sakib";
+
+    or,
+    multimap <string, int> v1;
+    v1["asif"] = 1;
+    v1["sakib"] = 2;
     */
 
 
-//can intialize map by using operator like[]
-     map <int, string> v4;
-     v4[1] = "asif";
-     v4[2] = "sakib";
-     v4[3] = "tamim";
-     v4[4] = "afif";
-     v4[5] = "liton";
-
-    cout<<"\nmap<int, string> v4\nid\tname\n";
-    for(auto i: v4)
-    {
-        cout<<i.first<<"\t"<<i.second<<endl;
-    }
-    cout<<endl;
-
-
-    //same as before v1
-    multimap<int, string> v2;
-    v2.insert(pair<int, string>(2,"sakib")); //same as v2.insert(make_pair(2,"sakib"));
-    v2.insert(pair<int, string>(1,"asif"));
-    v2.insert(pair<int, string>(5,"liton"));
-    v2.insert(pair<int, string>(3,"tamim"));
-    v2.insert(pair<int, string>(4,"afif"));
-
-
-    cout<<"\nmultimap<int, string> v2\nid\tname\n";
-    for(auto i: v2)
-    {
-        cout<<i.first<<"\t"<<i.second<<endl;
-    }
-    cout<<endl;
-
-
-    //or can be write same as use emplace instead of insert
+    //or
     multimap<int, string> v3;
-    v3.emplace(make_pair(2,"sakib")); //we can use make_pair instead of pair<int, string>
-    v3.emplace(make_pair(1,"asif"));
-    v3.emplace(make_pair(5,"liton"));
-    v3.emplace(make_pair(3,"tamim"));
-    v3.emplace(make_pair(4,"afif"));
+    v3.insert(make_pair(2,"sakib")); //same as -> v3.insert(pair<int, string>({2,"sakib"}));
+    v3.insert(make_pair(1,"asif"));
+    v3.insert(make_pair(5,"liton"));
+    v3.insert(make_pair(3,"tamim"));
+    v3.insert(make_pair(4,"afif"));
 
 
     cout<<"\nmultimap<int, string> v3\nid\tname\n";
@@ -100,42 +85,114 @@ int main()
     cout<<endl;
 
 
+    //or can be write same as use emplace instead of insert
+    multimap<int, string> v4;
+    v4.emplace(make_pair(2,"sakib")); //same as -> v4.emplace(pair<int, string>({2,"sakib"}));
+    v4.emplace(make_pair(1,"asif"));
+    v4.emplace(make_pair(5,"liton"));
+    v4.emplace(make_pair(3,"tamim"));
+    v4.emplace(make_pair(4,"afif"));
+
+    cout<<"\nmultimap<int, string> v4\nid\tname\n";
+    for(auto i: v4)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    //or using pair by using one pair variable
+    multimap<int, string> v5;
+    pair<int, string> p;
+    p = make_pair(2,"sakib"); // never write -> p(2, "sakib");
+    v5.insert(p);
+    p = make_pair(1,"asif");
+    v5.insert(p);
+    p = make_pair(5,"liton");
+    v5.insert(p);
+    p = make_pair(3,"tamim");
+    v5.insert(p);
+    p = make_pair(4,"afif");
+    v5.insert(p);
+
+    cout<<"\nmultimap<int, string> v5\nid\tname\n";
+    for(auto i: v5)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    //or using pair by using many pair variables
+    multimap<int, string> v6;
+    pair<int, string> p1 = make_pair(2,"sakib");  //same as -> pair<int, string> p1(2,"sakib");
+    v6.insert(p1);
+    pair<int, string> p2 = make_pair(1,"asif");
+    v6.insert(p2);
+    pair<int, string> p3 = make_pair(5,"liton");
+    v6.insert(p3);
+    pair<int, string> p4 = make_pair(3,"tamim");
+    v6.insert(p4);
+    pair<int, string> p5 = make_pair(4,"afif");
+    v6.insert(p5);
+
+    cout<<"\nmultimap<int, string> v6\nid\tname\n";
+    for(auto i: v6)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
 
 // iterator
     // print multimap element using iteration
-    multimap<int, string>  s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+    multimap<int, string> s = {{2,"sakib"},{1,"asif"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
-    multimap<int, string> ::iterator it;  // Creating the iterator.
+    multimap<int, string>::iterator it;  // Creating the iterator.
 
-    cout<<"multimap<int, string>  s \nid\tname\n";
+    cout<<"\nmultimap<int, string> s\nid\tname\n";
     for (it = s.begin(); it != s.end(); it++)
     {
     	cout<<it->first<<"\t"<<it->second<<endl;
     }
     cout<<endl;
 
-    /* same as before
-        multimap<int, string>  s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
-        cout<<"multimap<int, string>  s \nid\tname\n";
-        for (auto it = s.begin(); it != s.end(); it++)
-        {
-            cout<<it->first<<"\t"<<it->second<<endl;
-        }
-        cout<<endl;
+    /* same as above
+    multimap<int, string> s = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    multimap<int, string>::iterator it = s.begin();  // Creating the iterator.
+
+    cout<<"\nmultimap<int, string> s\nid\tname\n";
+    while(it != s.end())
+    {
+    	cout<<it->first<<"\t"<<it->second<<endl;
+    	it++;
+    }
+    cout<<endl;
     */
 
 
     // print multimap element without using iteration
-    multimap<int, string>  s1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+    multimap<int, string> s1 = {{2,"sakib"},{1,"asif"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
 
     cout<<"\nmultimap<int, string> s1\nid\tname\n";
-    for(auto i: s1)
+    for (auto i: s1)
     {
-        cout<<i.first<<"\t"<<i.second<<endl;
+    	cout<<i.first<<"\t"<<i.second<<endl;
     }
     cout<<endl;
 
+    /* can write also
+    multimap<int, string>  s1 = {{2,"sakib"},{1,"asif"},{5,"liton"},{3,"tamim"},{4,"afif"}};
+
+    cout<<"multimap<int, string>  s1 \nid\tname\n";
+    for (auto it = s1.begin(); it != s1.end(); it++)
+    {
+        cout<<it->first<<"\t"<<it->second<<endl;
+    }
+    cout<<endl;
+    */
 
 
 
@@ -158,49 +215,32 @@ int main()
     cout<<endl;
 
 
+
     //output:
-    //    enter the length of id numbers: 5
-    //    multimap<int, string>  v :
+    //    enter the length of multimap: 5
+    //    multimap<int, string> v :
+    //    id: 4
+    //    name: afif
+    //    id: 3
+    //    name: tamim
+    //    id: 5
+    //    name: liton
     //    id: 1
     //    name: asif
     //    id: 2
     //    name: sakib
-    //    id: 5
-    //    name: afif
-    //    id: 3
-    //    name: liton
-    //    id: 4
-    //    name: tamim
     //
     //
     //    multimap<int, string> v
     //    id      name
     //    1       asif
     //    2       sakib
-    //    3       liton
-    //    4       tamim
-    //    5       afif
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
     //
     //    multimap<int, string> v1
-    //    id      name
-    //    1       asif
-    //    2       sakib
-    //    3       tamim
-    //    4       afif
-    //    5       liton
-    //
-    //
-    //
-    //    map<int, string> v4
-    //    id      name
-    //    1       asif
-    //    2       sakib
-    //    3       tamim
-    //    4       afif
-    //    5       liton
-    //
-    //
-    //    multimap<int, string> v2
     //    id      name
     //    1       asif
     //    2       sakib
@@ -217,7 +257,35 @@ int main()
     //    4       afif
     //    5       liton
     //
-    //    multimap<int, string>  s
+    //
+    //    multimap<int, string> v4
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    multimap<int, string> v5
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    multimap<int, string> v6
+    //    id      name
+    //    1       asif
+    //    2       sakib
+    //    3       tamim
+    //    4       afif
+    //    5       liton
+    //
+    //
+    //    multimap<int, string> s
     //    id      name
     //    1       asif
     //    2       sakib
