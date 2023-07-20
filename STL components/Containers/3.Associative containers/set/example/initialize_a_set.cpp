76d249cb76ad319694ae48d008077fb99b8604f9
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-07-20 20:04:52 +0600

    initialize a set

--- STL components/Containers/3.Associative containers/set/example/initialize_a_set.cpp
+++ STL components/Containers/3.Associative containers/set/example/initialize_a_set.cpp
@@ -13,7 +13,7 @@ int main()
     cout<<"enter the length of set: ";
     cin>>len;
 
-    cout<<"set<int> v : \n";
+    cout<<"\nset<int> v : \n";
     for(int i=0; i<len; i++)
     {
         cin>>x;
@@ -21,7 +21,7 @@ int main()
     }
     cout<<endl;
 
-    cout<<"set<int> v = ";
+    cout<<"\nset<int> v: ";
     for(auto i: v)
     {
         cout<<i<<" ";
@@ -30,15 +30,15 @@ int main()
 
 
 // initialize in set
-    // Initializer List 
-    set <int> v1 = {10,20,50,40,30};   
-    
-    /* same as Uniform Initialization  
-    set <int> v1 {10,20,50,40,30};   
+    // Initializer List
+    set <int> v1 = {10,20,50,40,30};
+
+    /* same as Uniform Initialization
+    set <int> v1 {10,20,50,40,30};
     */
 
-    
-    cout<<"set<int> v1 = ";
+
+    cout<<"\nset<int> v1: ";
     for(auto i: v1)
     {
         cout<<i<<" ";
@@ -55,12 +55,13 @@ int main()
     }
     cout<<endl;
 
-    cout<<"set<int> v2 = ";
+    cout<<"\nset<int> v2: ";
     for(auto i: v2)
     {
         cout<<i<<" ";
     }
     cout<<endl;
+
 
     //or
     set<int> v3;
@@ -70,7 +71,7 @@ int main()
     v3.insert(7);
     v3.insert(6);
 
-    cout<<"\nset<int> v3 = ";
+    cout<<"\nset<int> v3: ";
     for(auto i: v3)
     {
         cout<<i<<" ";
@@ -86,12 +87,13 @@ int main()
     v4.emplace(6);
     v4.emplace(10);
 
-    cout<<"\nset<int> v4 = ";
+    cout<<"\nset<int> v4: ";
     for(auto i: v4)
     {
         cout<<i<<" ";
     }
     cout<<endl;
+
 
 
 // iterator
@@ -100,30 +102,67 @@ int main()
 
     set<int>::iterator it;  // Creating the iterator.
 
-    cout<<"set<int> s = ";
+    cout<<"\nset<int> s: ";
     for (it = s.begin(); it != s.end(); it++)
     {
     	cout<<*it<<" ";
     }
     cout<<endl;
 
+
+    /* same as above
+    set<int> s = {12,43,234,65,34,54,3};
+
+    set<int>::iterator it = s.begin();  // Creating the iterator.
+
+    cout<<"\nset<int> s: ";
+    while(it != s.end())
+    {
+    	cout<<*it<<" ";
+    	it++;
+    }
+    cout<<endl;
+    */
+
+
+
     // print set element without using iteration
     set<int> s1 = {12,43,234,65,34,54,3};
 
-    cout<<"set<int> s1 = ";
+    cout<<"\nset<int> s1: ";
     for (auto i: s1)
     {
     	cout<<i<<" ";
     }
     cout<<endl;
 
+    /* can write also
+    set<int> s1 = {12,43,234,65,34,54,3};
+
+    cout<<"\nset<int> s1: ";
+    for (auto it = s1.begin(); it != s1.end(); it++)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+    */
 
 
 
-//set can initialize in descending order
-    set<int, greater<int>> s2 = {10,30,40,20,50};
-    cout<<"set<int> s2 = ";
-    for (auto i: s2)
+
+//set can initialize in ascending order && descending order
+    set<int> s21 = {10,30,40,20,50}; //ascending order
+    cout<<"\n\nset<int> s21: ";
+    for (auto i: s21)
+    {
+    	cout<<i<<" ";
+    }
+    cout<<endl;
+
+
+    set<int, greater<int>> s22 = {10,30,40,20,50}; //descending order
+    cout<<"\nset<int> s22: ";
+    for (auto i: s22)
     {
     	cout<<i<<" ";
     }
@@ -136,14 +175,14 @@ int main()
     set <int> m = {10,20,50,40,30};
     set <int> m1(m);
 
-    cout<<"set<int> m = ";
+    cout<<"\n\nset<int> m: ";
     for(auto i: m)
     {
         cout<<i<<" ";
     }
     cout<<endl;
 
-    cout<<"after copy m into m1\nset<int> m1 = ";
+    cout<<"\nafter copy m into m1\nset<int> m1: ";
     for(auto i: m1)
     {
         cout<<i<<" ";
@@ -160,17 +199,28 @@ int main()
     //    30
     //
     //    set<int> v = 10 20 30 40
-    //    set<int> v1 = 10 20 30 40 50
     //
-    //    set<int> v2 = 1 2 3 4 5
     //
-    //    set<int> v3 = 6 7 8 9 10
+    //    set<int> v1: 10 20 30 40 50
     //
-    //    set<int> v4 = 6 7 8 9 10
-    //    set<int> s = 3 12 34 43 54 65 234
-    //    set<int> s1 = 3 12 34 43 54 65 234
-    //    set<int> s2 = 50 40 30 20 10
-    //    set<int> m = 10 20 30 40 50
+    //    set<int> v2: 1 2 3 4 5
+    //
+    //    set<int> v3: 6 7 8 9 10
+    //
+    //    set<int> v4: 6 7 8 9 10
+    //
+    //    set<int> s: 3 12 34 43 54 65 234
+    //
+    //    set<int> s1: 3 12 34 43 54 65 234
+    //
+    //
+    //    set<int> s21: 10 20 30 40 50
+    //
+    //    set<int> s22: 50 40 30 20 10
+    //
+    //
+    //    set<int> m: 10 20 30 40 50
+    //
     //    after copy m into m1
-    //    set<int> m1 = 10 20 30 40 50
+    //    set<int> m1: 10 20 30 40 50
 }
