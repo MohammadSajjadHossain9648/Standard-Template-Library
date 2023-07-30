4c01aaf602838e02ad3bef00be5e6ef163399597
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-07-30 19:47:36 +0600

    issue about insertion and search in unordered map

---
+++ STL components/Containers/4.Unordered associative containers/Unordered map/example/issue_about_insertion_and_search_in_a_unordered_map.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+/*
+    always remember: whatever you input in unordered_map, it store duplicate values.
+*/
+int main()
+{
+//what happen when inserting data in same key
+    unordered_map <int, string> v = {{1,"asif"},{2,"sakib"},{3,"tamim"},{4,"afif"},{5,"liton"}};
+
+    cout<<"\nunordered_map<int, string> v\nid\tname\n";
+    for(auto i: v)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+    //never insert the key cause already inserted in map but only update the key data in map
+    v[4] = "mehedi";
+
+    cout<<"\nunordered_map<int, string> v\nid\tname\n";
+    for(auto i: v)
+    {
+        cout<<i.first<<"\t"<<i.second<<endl;
+    }
+    cout<<endl;
+
+
+
+
+
+//what happen when searching key is not inserted
+    /* show error if write
+    cout<<"\n\nvalue in key 10: "<<v.at(10)<<endl;
+
+    or,
+    cout<<"\n\nvalue in key 10: "<<v.at(10)<<endl;
+    v[10];
+
+
+    output:
+    value in key 10: terminate called after throwing an instance of 'std::out_of_range'
+    what():  unordered_map::at
+    */
+
+
+    v[10]; // automatically create key 10 with null value
+    cout<<"\n\nvalue in key 10: "<<v.at(10)<<endl;  // nothing show error cause automatically create key 10 with null value
+}
